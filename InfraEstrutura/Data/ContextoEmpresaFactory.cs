b6543fc2bb6e48d26b15eeeb29a7b2cdbdd79cd4
anapaulazanelato <anapaulazanelato@gmail.com>
2025-08-18 21:36:04 -0300

    teste

--- InfraEstrutura/Data/ContextoEmpresaFactory.cs
+++ InfraEstrutura/Data/ContextoEmpresaFactory.cs
@@ -8,6 +8,7 @@ public class ContextoEmpresaFactory : IDesignTimeDbContextFactory<EmpresaContext
     {
         var optionsBuilder = new DbContextOptionsBuilder<EmpresaContexto>();
 
+
         // Defina a string de conexão de forma que o EF possa usar durante o processo de migração.
         optionsBuilder.UseSqlServer(@"Server=localhost;
                 DataBase=dbEmpresa2025;
